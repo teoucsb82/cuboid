cad9135a365ab790813712b1d200241c0d10e4f9
Teo Dell'Amico <teo.dellamico@gmail.com>
2017-04-21 21:04:59 -0700

    Added tests for Cuboid#intersects?

--- spec/cuboid_spec.rb
+++ spec/cuboid_spec.rb
@@ -33,6 +33,61 @@ describe Cuboid do
   end    
   
   describe "intersects?" do
+    let(:cuboid_1_origin) { { x: 0, y: 0, z: 0 } } 
+    let(:cuboid_1_length) { 10 }
+    let(:cuboid_1_width) { 10 }
+    let(:cuboid_1_height) { 10 }
+    let(:cuboid_1) { Cuboid.new(cuboid_1_origin, cuboid_1_length, cuboid_1_width, cuboid_1_height) }
+
+    let(:cuboid_2_length) { 5 }
+    let(:cuboid_2_width) { 5 }
+    let(:cuboid_2_height) { 5 }
+    let(:cuboid_2) { Cuboid.new(cuboid_2_origin, cuboid_2_length, cuboid_2_width, cuboid_2_height) }
+
+    context 'totally separate cuboids' do
+      let(:cuboid_2_origin) { { x: 99, y: 99, z: 99 } } 
+
+      it { expect(cuboid_1.intersects?(cuboid_2)).to eq false }
+      it { expect(cuboid_2.intersects?(cuboid_1)).to eq false }
+    end
+
+    context 'adjacent cuboids' do
+      context 'end to end cuboids' do
+        let(:cuboid_2_origin) { { x: cuboid_1_length, y: 0, z: 0 } } 
+
+        it { expect(cuboid_1.intersects?(cuboid_2)).to eq false }
+        it { expect(cuboid_2.intersects?(cuboid_1)).to eq false }
+      end
+
+      context 'side by side cuboids' do
+        let(:cuboid_2_origin) { { x: 0, y: 0, z: cuboid_1_width } } 
+
+        it { expect(cuboid_1.intersects?(cuboid_2)).to eq false }
+        it { expect(cuboid_2.intersects?(cuboid_1)).to eq false }
+      end
+
+      context 'vertically stacked cuboids' do
+        let(:cuboid_2_origin) { { x: 0, y: cuboid_1_height, z: 0 } } 
+
+        it { expect(cuboid_1.intersects?(cuboid_2)).to eq false }
+        it { expect(cuboid_2.intersects?(cuboid_1)).to eq false }
+      end
+    end
+
+    context 'overlapping cuboids' do
+      let(:cuboid_2_origin) { { x: 1, y: 1, z: 1 } } 
+      
+      it { expect(cuboid_1.intersects?(cuboid_2)).to eq true }
+      it { expect(cuboid_2.intersects?(cuboid_1)).to eq true }
+    end
+
+    context 'one cuboid inside another' do
+      let(:cuboid_1_origin) { { x: 0, y: 0, z: 0 } } 
+      let(:cuboid_2_origin) { { x: 2, y: 2, z: 2 } } 
+
+      it { expect(cuboid_1.intersects?(cuboid_2)).to eq true }
+      it { expect(cuboid_2.intersects?(cuboid_1)).to eq true }
+    end
   end
 
   describe "vertices" do
